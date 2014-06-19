ba7112ba5a244fbd44227048944f75e9e2819b69
Jarvys <yuhan534@126.com>
2014-06-19 12:02:29 +0800

    导入学校信息

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
+# coding: utf-8
 from django.db import models
 
 # Create your models here.
+
+class Region(models.Model):
+    name = models.CharField(verbose_name=u'name', max_length=80, unique=True)
+
+
+class City(models.Model):
+    region = models.ForeignKey(Region)
+    name = models.CharField(verbose_name=u'name', max_length=80, unique=True)
+
+
+class University(models.Model):
+    city = models.ForeignKey(City)
+    name = models.CharField(verbose_name=u'name', max_length=100, unique=True)
